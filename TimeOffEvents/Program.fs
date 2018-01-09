fba57a194cb8ef495e4923c3734cb9b37a8259cc
andreESGI <andre92.morel@gmail.com>
2018-01-09 20:26:27 +0100

    push modif

--- TimeOffEvents/Program.fs
+++ TimeOffEvents/Program.fs
@@ -1,6 +1,13 @@
 module TimeOff.TestsRunner
 
+open System
 open Expecto
+
+let newEmployee = User.Employee 1;
+
+let name = System.Console.ReadLine()
+
+printfn "name"
 
 [<EntryPoint>]
 let main args =
