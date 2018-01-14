ada0e3b829a8625e2379bc59bf61e136f57ebf67
andreESGI <andre92.morel@gmail.com>
2018-01-14 10:48:39 +0100

    Generic repo

--- TimeOffEvents/Program.fs
+++ TimeOffEvents/Program.fs
@@ -22,7 +22,7 @@ module TimeOff =
 
         let app =
             choose [
-                Restful.TimeOffWebPart repository
+                Restful.TimeOffWebPart "TimeOff" repository
                 RequestErrors.NOT_FOUND "Found no handlers"
             ]
 
